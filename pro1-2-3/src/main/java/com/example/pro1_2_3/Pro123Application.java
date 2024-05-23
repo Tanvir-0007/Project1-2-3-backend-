262889b675e5835edc449c04d9e3ee78d218044c
Tanvir-0007 <tanvir.bracit@gmail.com>
2024-05-23 14:41:16 +0600

    Add files via upload

---
+++ pro1-2-3/src/main/java/com/example/pro1_2_3/Pro123Application.java
@@ -0,0 +1,13 @@
+package com.example.pro1_2_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pro123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pro123Application.class, args);
+	}
+
+}
